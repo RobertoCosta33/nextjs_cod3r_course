c48edba196d09208e904755ed9d9ac75387e2626
Roberto B Costa <romeromatallica@gmail.com>
2022-03-27 08:57:56 -0300

    feat: create components and add props from all

---
+++ pages/exemple.tsx
@@ -0,0 +1,12 @@
+import Header from "../src";
+
+const Exemple: React.FC = () => {
+  return (
+    <>
+      <Header title="Next.Js & React" />
+      <Header title="Aprenda Next.Js Na Prática" />
+    </>
+  );
+};
+
+export default Exemple;
